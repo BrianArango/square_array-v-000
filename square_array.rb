51d36a2cc9029f4c72e62a8248e21a43725e1b93
Brian Arango <brianvictus@gmail.com>
2020-07-25 23:13:32 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,26 @@
 def square_array(array)
-
- new_array = []
-
- array.each do |integer|
-  new_array << integer ** 2
- end
- return new_array
+  new_array = []
+  array.each do |integer|
+    new_array << integer ** 2
+  end
+  return new_array
 end
+
+def square_numbers(numbers)
+  new_numbers = numbers.collect{|num| num ** 2}
+  return new_numbers
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
